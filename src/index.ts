43a81db82adda37ac81af7e0558da2343e09cd11
karl <ktaylor@solomonic.co.uk>
2024-08-08 16:39:11 +0100

    Wtf

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("yeet")
+#!/usr/bin/env node
+
+import { exec, execSync } from "child_process";
+
+console.log("yeet");
+
+console.log(process.cwd());
+
+const cwd = process.cwd();
+
+const rootDir = cwd.split("/")[cwd.split("/").length - 1];
+
+console.log({ rootDir });
+
+const devScriptMap = {
+  v2: "dev",
+  "rollup-service": "up:dev",
+  "user-service": "up:user-stack:dev",
+};
+
+exec(`yarn ${devScriptMap[rootDir as keyof typeof devScriptMap]}`, (err, stdout, stderr) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(stdout);
+})
